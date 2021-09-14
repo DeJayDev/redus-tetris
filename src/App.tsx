bf741904c10fa78025ce3dd32a6577233877e9e8
DeJayDev <contact@dejaydev.com>
2021-09-13 19:12:52 -0500

    Added state utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
 import './App.css';
+
 import Controls from './components/Controls';
 import GridBoard from './components/GridBoard';
 import MessagePopup from './components/MessagePopup';
 import NextBlock from './components/NextBlock';
 import Scoreboard from './components/Scoreboard';
 
+const store = createStore(reducers)
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetrux</h1>
-      </header>
-      <GridBoard/>
-      <NextBlock/>
-      <Scoreboard/>
-      <Controls />
-      <MessagePopup />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetrux</h1>
+        </header>
+        <GridBoard/>
+        <NextBlock/>
+        <Scoreboard/>
+        <Controls />
+        <MessagePopup />
+      </div>
+    </Provider>
   );
 }
 
