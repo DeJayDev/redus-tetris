c7963c7a29dac865e99f6fb6ffc4d21f93c63b0e
DeJayDev <contact@dejaydev.com>
2021-09-13 20:03:57 -0500

    Added connection for message popup

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,27 +1,34 @@
 import { Fragment } from 'react'
 import '../css/Controls.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
+import { RootState } from '../reducers'
+
 
 function Controls(props: any) {
+	const dispatch = useDispatch()
+	const isRunning = useSelector((state: RootState) => state.game.isRunning)
+
 	return <Fragment>
 		<div className="controls">
 			{/* left */}
 			<button className="control-button" onClick={(e) => {
-
+				dispatch(moveLeft())
 			}}>Left</button>
 
 			{/* right */}
 			<button className="control-button" onClick={(e) => {
-
+				dispatch(moveRight())
 			}}>Right</button>
 
 			{/* rotate */}
 			<button className="control-button" onClick={(e) => {
-
+				dispatch(rotate())
 			}}>Rotate</button>
 
 			{/* down */}
 			<button className="control-button" onClick={(e) => {
-
+				dispatch(moveDown())
 			}}>Down</button>
 
 		</div>
--- src/components/MessagePopup.tsx
+++ src/components/MessagePopup.tsx
@@ -1,9 +1,25 @@
 import { Fragment } from 'react'
+import { useSelector } from 'react-redux'
 import '../css/MessagePopup.css'
+import { RootState } from '../reducers'
 
 function MessagePopup(props: any) {
+	const isRunning = useSelector((state: RootState) => state.game.isRunning)
+	const gameOver = useSelector((state: RootState) => state.game.gameOver)
+
+	let message = ''
+	let isHidden = 'hidden'
+  
+	if (gameOver) {
+		message = 'Game Over'
+		isHidden = ''
+	} else if (!isRunning) {
+		message = 'Paused'
+		isHidden = ''
+	}
+
 	return <Fragment>
-		<div className="message-popup">
+		<div className={`message-popup ${isHidden}`}>
 			<h1>Message Title</h1>
 			<p>Message Content</p>
 		</div>
